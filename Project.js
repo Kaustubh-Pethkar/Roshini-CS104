395baa2da75b67c3d9b2e7c1556efcf6ab065e71
Kaustubh <emailsforkaustubh@gmail.com>
2024-12-07 06:10:29 +0530

    Answer

---
+++ Project.js
@@ -0,0 +1,22 @@
+
+function cart1DataSave(productName){
+    localStorage.setItem('saveProduct', productName);
+    window.location.href = 'Buy.html';
+}
+
+function cart2DataSave(productName){
+    localStorage.setItem('saveProduct', productName);
+    window.location.href = 'Buy.html';
+}
+
+
+function cart3DataSave(productName){
+    localStorage.setItem('saveProduct', productName);
+    window.location.href = 'Buy.html';
+}
+
+function cart4DataSave(productName){
+    localStorage.setItem('saveProduct', productName);
+    window.location.href = 'Buy.html';
+}
+
